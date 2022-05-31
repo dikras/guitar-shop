db3e1b384c9193c70127d2e51f8f92d1bc8c1cf9
Dmitry Krasyukov <dmi1517@yandex.ru>
2022-05-31 18:10:14 +0300

    заменил адрес сервера

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosInstance} from 'axios';
 
-const BACKEND_URL = 'https://accelerator-guitar-shop-api-v1.glitch.me';
+const BACKEND_URL = 'https://guitar-shop.accelerator.pages.academy';
 const REQUEST_TIMEOUT = 5000;
 
 const createAPI = (): AxiosInstance => {
